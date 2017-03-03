6c47777e4d8764d2c39954033e5d47664198db1d
Nicolas Antor <n.antor@outlook.com>
2017-03-03 16:12:16 +0900

    Added todo lists for array and map

---
+++ ts/dictionary/map.ts
@@ -0,0 +1,14 @@
+/**
+ * Extends the new JS Map type https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
+ * to ensure runtime typechecking on all function which mutate state
+ */
+export class TMap<K, V> extends Map<K, V> {
+    /**
+     * TODO:
+     * delete()
+     * forEach()
+     * get()
+     * has()
+     * set()
+     */
+}
--- ts/list/array.ts
+++ ts/list/array.ts
@@ -1,5 +1,9 @@
 import { checkPrimitiveTypes } from '../typecheckers/primitives'
 
+/**
+ * Extends the JavaScript Array to ensure runtime typechecking for all array function mutating state.
+ * Other functions will still be handled by the Array<T> implementation
+ */
 export class TArray<T> extends Array<T> {
     constructor(start: Array<T> = []) {
         super();
@@ -10,4 +14,35 @@ export class TArray<T> extends Array<T> {
         checkPrimitiveTypes(this, typeof values);
         return this.push(...values);
     } 
+
+    /**
+     * TODO:
+     * isArray()
+     * concat()
+     * copyWithin()
+     * entries()
+     * every()
+     * fill()
+     * filter()
+     * find()
+     * findIndex()
+     * forEach()
+     * includes()
+     * indexOf()
+     * join()
+     * keys()
+     * lastIndexOf()
+     * map()
+     * reduce()
+     * reduceRight()
+     * slice()
+     * some()
+     * sort()
+     * slice()
+     * splice()
+     * toLocaleString()
+     * toSource()
+     * unshift()
+     * valuesOf()
+     */
 }
