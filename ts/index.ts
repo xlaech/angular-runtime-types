50060e34c6a93a525ea0f9e97b5b109a472c6be3
Nicolas Antor <n.antor@outlook.com>
2017-03-03 11:07:51 +0900

    Created new Project with first Test Class

---
+++ ts/index.ts
@@ -0,0 +1,7 @@
+// Sample exports from other modules
+//export * from "./deep/foo";
+//export * from "./deep/deeper/bar";
+export * from "./primitives/primitives";
+
+// Sample export of our own
+export var bas = "123";
