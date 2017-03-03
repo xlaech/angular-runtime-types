b5d979b61839260fb166379953153da93e381598
Nicolas Antor <n.antor@outlook.com>
2017-03-03 15:11:14 +0900

    Added Tests for TBool and Documentation in primitives.ts

--- tests/primitives/primitives.test.ts
+++ tests/primitives/primitives.test.ts
@@ -11,6 +11,48 @@ describe('Primitives:', () => {
 
         it('throws for a null value at initialization', () => {
             expect(() => new TInt(null)).throws();
+        });
+
+        it('will accept and save a new integer value', () => {
+            let val: TInt = new TInt(3);
+            val.value = 42;
+            expect(val.value).to.equal(42);
+        });
+
+        it('throws for a new null value', () => {
+            let val: TInt = new TInt(42);
+            expect(() => val.value = null).throws();
+        });
+
+        it('throws for a new non number value', () => {
+            let val: any = new TInt(42);
+            expect(() => val.value = "test").throws();
+        })
+    });
+    describe("TBool:", () => {
+        it('can be created and its value can be set and is saved', () => {
+            let val: TBool = new TBool(true);
+            expect(val.value).to.equal(true);
+        });
+
+        it('throws for a null value at initialization', () => {
+            expect(() => new TBool(null)).throws();
+        });
+
+        it('will accept and save a new integer value', () => {
+            let val: TBool = new TBool(true);
+            val.value = false;
+            expect(val.value).to.equal(false);
+        });
+
+        it('throws for a new null value', () => {
+            let val: TBool = new TBool(true);
+            expect(() => val.value = null).throws();
+        });
+
+        it('throws for a new non number value', () => {
+            let val: any = new TBool(true);
+            expect(() => val.value = "test").throws();
         })
     })
 });
--- ts/primitives/primitives.ts
+++ ts/primitives/primitives.ts
@@ -1,8 +1,18 @@
 import {checkPrimitiveTypes} from "../typecheckers/primitives"
 
+/**
+ * Abstract generic class encapsulating the value of a primitive T.
+ */
 export abstract class Typesafe<T> {
+    /**
+     * The value we are encapsulating
+     */
     protected _value: T;
 
+    /**
+     * Reading the value is a safe operation. So we just return it.
+     * @returns T The value
+     */
     get value(): T {
         return this._value;
     }
@@ -13,6 +23,11 @@ export abstract class Typesafe<T> {
     }
 }
 
+/**
+ * Specific instance of Typesafe<T> for number values.
+ * see commit cbc6b700ace6f42f3a31a889a7cca5c8d1723011 for information
+ * about why the constructor is not in the generic class
+ */
 export class TInt extends Typesafe<number> {
     constructor(start: number) {
         super();
@@ -21,6 +36,24 @@ export class TInt extends Typesafe<number> {
     }
 };
 
+/**
+ * Specific instance of Typesafe<T> for string values.
+ */
+export class TString extends Typesafe<string> {
+    constructor(start: string) {
+        super();
+        checkPrimitiveTypes("string", start)
+        this._value = start;
+    }
+};
 
-export class TString extends Typesafe<string> {};
-export class TBool extends Typesafe<boolean> {};
+/**
+ * Specific instance of Typesafe<T> for boolean values.
+ */
+export class TBool extends Typesafe<boolean> {
+    constructor(start: boolean) {
+        super();
+        checkPrimitiveTypes(true, start)
+        this._value = start;
+    }
+};
